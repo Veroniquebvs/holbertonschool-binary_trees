74c626b5320eca1a1784be7a6027b62571bde611
Veroniquebvs <beauvais.veronique@gmail.com>
2025-12-09 14:39:29 +0000

    fix : change order of condition

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -11,7 +11,14 @@
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
+    binary_tree_t *new_node;
+    
+    if (parent == NULL)
+    {
+        return (NULL);
+    }
+
+    new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
 	{
@@ -22,11 +29,6 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->parent = parent;
 	new_node->left = parent->left;
 	new_node->right = NULL;
-    
-    if (parent == NULL)
-    {
-        return (NULL);
-    }
 
     if (parent->left != NULL)
     {

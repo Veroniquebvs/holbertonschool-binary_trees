17bb430e9ada59dd07aba1be2afb3f88e2eb66de
Veroniquebvs <beauvais.veronique@gmail.com>
2025-12-09 14:47:33 +0000

    fix : correction of errors betty style

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -6,23 +6,23 @@
  * @parent: pointer to the node to insert the right-child in
  * @value :  the value to store in the new node
  *
- *Return: a pointer to the created node, or NULL on failure or if parent is NULL
+ *Return: a pointer to the created node, or NULL
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-    binary_tree_t *new_node;
-    
-    if (parent == NULL)
-    {
-        return (NULL);
-    }
+	binary_tree_t *new_node;
 
-    new_node = malloc(sizeof(binary_tree_t));
+	if (parent == NULL)
+	{
+		return (NULL);
+	}
+
+	new_node = malloc(sizeof(binary_tree_t));
 
 	if (new_node == NULL)
 	{
-		return(NULL);
+		return (NULL);
 	}
 
 	new_node->n = value;
@@ -30,12 +30,12 @@ binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 	new_node->left = parent->left;
 	new_node->right = NULL;
 
-    if (parent->left != NULL)
-    {
-        parent->left->parent = new_node;
-    }
+	if (parent->left != NULL)
+	{
+		parent->left->parent = new_node;
+	}
 
-    parent->left = new_node;
+	parent->left = new_node;
 
 	return (new_node);
 }
